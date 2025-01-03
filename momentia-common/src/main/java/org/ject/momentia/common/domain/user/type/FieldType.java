bebb6c080a6d133fe0070097392d86a4e417e0aa
RhoSeungA <sskclara@naver.com>
2025-01-03 22:55:36 +0900

    feat : 작업 분야, 작품 카테고리 반환시 한국어 이름으로 반환하는 거롤 수정

--- momentia-common/src/main/java/org/ject/momentia/common/domain/user/type/FieldType.java
+++ momentia-common/src/main/java/org/ject/momentia/common/domain/user/type/FieldType.java
@@ -1,13 +1,24 @@
 package org.ject.momentia.common.domain.user.type;
 
 public enum FieldType {
-    PAINTING,
-    CRAFTSCULPTURE,
-    DRAWING,
-    PRINTMAKING,
-    CALLIGRAPHY,
-    ILLUSTRATION,
-    DIGITALART,
-    PHOTOGRAPHY,
-    OTHERS
+    PAINTING("회화"),
+    CRAFTSCULPTURE("공예/조각"),
+    DRAWING("드로잉"),
+    PRINTMAKING("판화"),
+    CALLIGRAPHY("서예"),
+    ILLUSTRATION("일러스트"),
+    DIGITALART("디지털아트"),
+    PHOTOGRAPHY("사진"),
+    OTHERS("기타");
+
+    private final String koreanName;
+
+    FieldType(String koreanName) {
+        this.koreanName = koreanName;
+    }
+
+    public String getKoreanName() {
+        return koreanName;
+    }
+
 }
