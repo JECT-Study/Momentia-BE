882fb7bfdfefdbe23d867eea1ff53e15ff21ca97
RhoSeungA <sskclara@naver.com>
2025-01-21 00:46:40 +0900

    fix: 작품 삭제 시 관련 댓글 전부 삭제 버그 수정

--- momentia-common/src/main/java/org/ject/momentia/common/domain/artwork/ArtworkComment.java
+++ momentia-common/src/main/java/org/ject/momentia/common/domain/artwork/ArtworkComment.java
@@ -1,11 +1,25 @@
 package org.ject.momentia.common.domain.artwork;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.*;
 import org.ject.momentia.common.domain.base.BaseEntity;
 import org.ject.momentia.common.domain.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
@@ -14,28 +28,43 @@ import org.ject.momentia.common.domain.user.User;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ArtworkComment extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", nullable = false)
+	private Long id;
 
-    @Size(max = 500)
-    @NotNull
-    @Column(name = "content", nullable = false, length = 500)
-    private String content;
+	@Size(max = 500)
+	@NotNull
+	@Column(name = "content", nullable = false, length = 500)
+	private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="post_id",referencedColumnName = "id",nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    @NotNull
-    private ArtworkPost post;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id", referencedColumnName = "id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	@NotNull
+	private ArtworkPost post;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id",referencedColumnName = "id",nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
-    @NotNull
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+	@NotNull
+	private User user;
 
-    public void update(String content){
-        this.content = content;
-    }
+	public void update(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		ArtworkComment that = (ArtworkComment)o;
+		return id != null && id.equals(that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
 
 }
